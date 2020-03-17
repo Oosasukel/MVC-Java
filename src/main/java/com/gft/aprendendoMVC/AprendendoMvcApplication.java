48f058ca73083458b92ad6742bf2fe5d80ef4c5d
Oosasukel <rodrigo_gonn@hotmail.com>
2020-03-17 18:14:43 -0300

    Template com thymeleaf

--- src/main/java/com/gft/aprendendoMVC/AprendendoMvcApplication.java
+++ src/main/java/com/gft/aprendendoMVC/AprendendoMvcApplication.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
 @SpringBootApplication
 public class AprendendoMvcApplication {
 
@@ -18,6 +20,11 @@ public class AprendendoMvcApplication {
 	@Bean
 	public LocaleResolver localeResolver() {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+	
+	@Bean
+	public LayoutDialect layoutDialect() {
+	    return new LayoutDialect();
 	}
 
 }
