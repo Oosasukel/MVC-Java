6eddbf04ee584ec70afe2c98687b89150dfbcac7
Oosasukel <rodrigo_gonn@hotmail.com>
2020-03-17 14:54:42 -0300

    Tela novo titulo

---
+++ src/main/java/com/gft/aprendendoMVC/AprendendoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.gft.aprendendoMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoMvcApplication.class, args);
+	}
+
+}
