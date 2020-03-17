81244b5f0ee4cd5ebe45174eb67ddaa697609203
Oosasukel <rodrigo_gonn@hotmail.com>
2020-03-17 20:27:52 -0300

    Implementando edição

--- src/main/java/com/gft/aprendendoMVC/controller/TituloController.java
+++ src/main/java/com/gft/aprendendoMVC/controller/TituloController.java
@@ -2,13 +2,18 @@ package com.gft.aprendendoMVC.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gft.aprendendoMVC.model.StatusTitulo;
 import com.gft.aprendendoMVC.model.Titulo;
@@ -17,27 +22,36 @@ import com.gft.aprendendoMVC.repository.Titulos;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-	
+
 	@Autowired
 	private Titulos titulos;
-	
+
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject(new Titulo());
 		return mv;
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo) {
-		
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+		if (errors.hasErrors()) {
+			return "CadastroTitulo";
+		}
 		titulos.save(titulo);
-		
+
+		attributes.addFlashAttribute("mensagem", "Titulo salvo com sucesso!");
+
+		return "redirect:/titulos/novo";
+	}
+
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem", "Titulo salvo com sucesso!");
-		
+		mv.addObject(titulo);
 		return mv;
 	}
-	
+
 	@RequestMapping
 	public ModelAndView pesquisar() {
 		List<Titulo> todosTitulos = titulos.findAll();
@@ -45,9 +59,9 @@ public class TituloController {
 		mv.addObject("titulos", todosTitulos);
 		return mv;
 	}
-	
+
 	@ModelAttribute("todosStatusTitulo")
-	public List<StatusTitulo> todosStatusTitulo(){
+	public List<StatusTitulo> todosStatusTitulo() {
 		return Arrays.asList(StatusTitulo.values());
 	}
 }
