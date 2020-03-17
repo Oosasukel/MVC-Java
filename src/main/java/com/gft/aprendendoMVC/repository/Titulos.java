4a2fb44002089b01b70efb6ac8087306625b43ee
Oosasukel <rodrigo_gonn@hotmail.com>
2020-03-17 16:37:32 -0300

    Cadastrando Titulo

---
+++ src/main/java/com/gft/aprendendoMVC/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.gft.aprendendoMVC.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gft.aprendendoMVC.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long>{
+
+}
